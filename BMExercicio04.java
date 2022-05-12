c077a62f6b7fa860f36d6f9f1ea39048514e4589
Naline Belo <62017582+Nahline@users.noreply.github.com>
2022-05-11 21:38:10 -0300

    Create BMExercicio04.java

---
+++ BMExercicio04.java
@@ -0,0 +1,21 @@
+//A soma não se torna possível pois são tipos diferentes de variáveis.
+
+// class BMExercicio04 {  
+//     public static void main(String args[]) { 
+//     long long1 = 100;
+//     int int1 = 200;
+//     int int2 = long1 + int1;
+//     System.out.println("int2 é: " + int2);  
+//   } 
+// }
+
+// A forma correta seria: 
+
+class BMExercicio04 {  
+    public static void main(String args[]) { 
+    int long1 = 100;
+    int int1 = 200;
+    int int2 = long1 + int1;
+    System.out.println("int2 é: " + int2);  
+  } 
+}
