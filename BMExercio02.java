7c74c3b2eeddf4f1f7295fc2a7566e44e9a77ad9
Naline Belo <62017582+Nahline@users.noreply.github.com>
2022-05-11 21:36:44 -0300

    Create BMExercio02.java

---
+++ BMExercio02.java
@@ -0,0 +1,22 @@
+class BMExercio02 {  
+  public static void main(String args[]) { 
+    int i; 
+    long j;
+    i = 1;
+    j = 2;
+    float p;
+    double q;
+    p = 20.0f;
+    q = 30.0;
+    Boolean b;
+    char c;
+    b = true;
+    c = 'k';
+    System.out.println(i);  
+    System.out.println(j);  
+    System.out.println(p);  
+    System.out.println(q);  
+    System.out.println(b);  
+    System.out.println(c);  
+  } 
+}
